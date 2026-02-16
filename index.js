52424b32b925d435e1723821f70161045e0f9049
vikramyadav6970-cell <vikramyadav6970@gmail.com>
2026-02-16 19:50:31 +0530

    Added total button and few improvements

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let count=0
 
+let sum=0
+
 let countp=document.getElementById("countp")
 
 let button=document.getElementById("button")
@@ -10,6 +12,10 @@ let saveb=document.getElementById("save-btn")
 
 let restartb=document.getElementById("restart-btn")
 
+let totalb=document.getElementById("total-btn")
+
+let totalp=document.getElementById("total-txt")
+
 function increment(){
     count++
     countp.innerHTML=count
@@ -17,6 +23,7 @@ function increment(){
 
 function Save(){
     savep.append(`${count} - `)
+    sum+=count
 }
 
 function Restart(){
@@ -24,8 +31,15 @@ function Restart(){
     countp.innerHTML=count
 }
 
+function Total(){
+    totalp.append(`${sum}`)
+    sum=0
+}
+
 button.addEventListener("click",increment)
 
 saveb.addEventListener("click",Save)
 
 restartb.addEventListener("click",Restart)
+
+totalb.addEventListener("click",Total)
