229663885540aec785dacf578a914345f82ff014
vikramyadav6970-cell <vikramyadav6970@gmail.com>
2026-02-16 19:25:23 +0530

    Added save and restart buttons

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ let countp=document.getElementById("countp")
 
 let button=document.getElementById("button")
 
+let savep=document.getElementById("save-txt")
+
+let saveb=document.getElementById("save-btn")
+
+let restartb=document.getElementById("restart-btn")
+
 function increment(){
     count++
     countp.innerHTML=count
 }
 
+function Save(){
+    savep.append(`${count} - `)
+}
+
+function Restart(){
+    count = 0
+    countp.innerHTML=count
+}
+
 button.addEventListener("click",increment)
+
+saveb.addEventListener("click",Save)
+
+restartb.addEventListener("click",Restart)
